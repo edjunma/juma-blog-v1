ee514c1ca07297f6baed18ea15f1c7717b84b59a
edjunma <edjunma@gmail.com>
2022-05-15 17:05:22 -0400

    finish jsx for home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
 
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing'},
@@ -15,15 +16,13 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
-            <div className='lg:sticky relative top-8'></div>
+            <div className='lg:sticky relative top-8'>
+              <PostWidget />
+              <Categories />
+            </div>
         </div>
       </div>
 
