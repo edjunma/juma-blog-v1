1bf386de7134fed396e3c5fd096d25ff7f01edec
edjunma <edjunma@gmail.com>
2022-05-16 04:14:20 -0400

    add layout.jsx to header component

---
+++ components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from './';
+
+<Layout>
+  sadasdasdasd
+</Layout>
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
+
+export default Layout
