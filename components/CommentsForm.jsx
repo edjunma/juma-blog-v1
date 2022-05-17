c70360c553695324d83e48ec7b8f05c894fc3dc4
edjunma <edjunma@gmail.com>
2022-05-17 03:26:02 -0400

    finish post details component, start on commentsform section

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const CommentsForm = () => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
   return (
-    <div>CommentsForm</div>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h1>CommentsForm</h1>
+    </div>
   )
 }
 
